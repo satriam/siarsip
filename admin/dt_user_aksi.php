ec415feaa53f866a782fef36c056fc4a40265f5b
mfaang11 <muhammadfarhanaang1@gmail.com>
2022-09-12 11:10:14 +0700

    update data

--- admin/dt_user_aksi.php
+++ admin/dt_user_aksi.php
@@ -1,7 +1,8 @@
 <?php 
 include '../koneksi.php'; 
 $nama  = $_POST['nama'];
-$ttl = $_POST['ttl'];
+$tempat = $_POST['tempat_lahir'];
+$tanggal = $_POST['tanggal_lahir'];
 $goldar = $_POST['goldar'];
 $nik = $_POST['nik'];
 $status = $_POST['status_kawin'];
@@ -22,8 +23,9 @@ $id_jj = $_POST['id_jj'];
 $nakes = $_POST['nakes'];
 $jenis_jabatan= $_POST['jenis_jabatan'];
 
-$data=mysqli_query($koneksi, "insert into diri values (NULL,'$nama','$ttl','$goldar','$nik','$status','$alamat'
+$data=mysqli_query($koneksi, "insert into diri values (NULL,'$nama','$tempat','$tanggal','$goldar','$nik','$status','$alamat'
 ,'$email','$npwp','$nip','$telpon1','$telpon2','$bpjs','$serata','$jurusan','$univ','$id_lokasi','$id_status','$id_jabatan','$id_jj','$nakes','$jenis_jabatan')");
+
 
 
 header("location:dt_user.php");
