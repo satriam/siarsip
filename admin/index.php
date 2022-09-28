7afed182d25f3815a5393b721df2595587c8b393
satriam <satriabae43@gmail.com>
2022-09-28 11:14:01 +0700

    update v4

--- admin/index.php
+++ admin/index.php
@@ -72,29 +72,30 @@
 
                     <?php
  // skrip koneksi database
- mysql_connect("localhost","root","");
- mysql_select_db("siarsip");
+//  mysql_connect("localhost","root","");
+//  mysql_select_db("siarsip");
  
  $ip      = $_SERVER['REMOTE_ADDR']; // Mendapatkan IP komputer user
  $tanggal = date("Ymd"); // Mendapatkan tanggal sekarang
  $waktu   = time(); //
   
  // Mencek berdasarkan IPnya, apakah user sudah pernah mengakses hari ini
- $s = mysql_query("SELECT * FROM tstatistika WHERE ip='$ip' AND tanggal='$tanggal'");
+ $s = mysqli_query($koneksi,"SELECT * FROM tstatistika WHERE ip='$ip' AND tanggal='$tanggal'");
  
  // Kalau belum ada, simpan data user tersebut ke database
- if(mysql_num_rows($s) == 0){
-     mysql_query("INSERT INTO tstatistika(ip, tanggal, hits, online) VALUES('$ip','$tanggal','1','$waktu')");
+ if(mysqli_num_rows($s) == 0){
+     mysqli_query($koneksi,"INSERT INTO tstatistika(ip, tanggal, hits, online) VALUES('$ip','$tanggal','1','$waktu')");
  }
  // Jika sudah ada, update
  else{
-     mysql_query("UPDATE tstatistika SET hits=hits+1, online='$waktu' WHERE ip='$ip' AND tanggal='$tanggal'");
+     mysqli_query($koneksi,"UPDATE tstatistika SET hits=hits+1, online='$waktu' WHERE ip='$ip' AND tanggal='$tanggal'");
  }
  
- $pengunjung       = mysql_num_rows(mysql_query("SELECT * FROM tstatistika WHERE tanggal='$tanggal' GROUP BY ip")); // Hitung jumlah pengunjung
- $totalpengunjung  = mysql_result(mysql_query("SELECT COUNT(hits) FROM tstatistika"), 0); // hitung total pengunjung
+ $pengunjung       = mysqli_num_rows(mysqli_query($koneksi,"SELECT * FROM tstatistika WHERE tanggal='$tanggal' GROUP BY ip")); // Hitung jumlah pengunjung
+ $hitung  = mysqli_query($koneksi,"SELECT COUNT(hits) FROM tstatistika"); // hitung total pengunjung
+ $totalpengunjung  = mysqli_free_result($hitung); // hitung total pengunjung
  $bataswaktu       = time() - 300;
- $pengunjungonline = mysql_num_rows(mysql_query("SELECT * FROM tstatistika WHERE online > '$bataswaktu'")); // hitung pengunjung online
+ $pengunjungonline = mysqli_num_rows(mysqli_query($koneksi,"SELECT * FROM tstatistika WHERE online > '$bataswaktu'")); // hitung pengunjung online
  ?> 
  
 
